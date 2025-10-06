092e92ff05dd3e73cb2ce212d6c5c34a820ca938
jimmymuthoni <jimmymuthoni26@gmail.com>
2025-10-06 13:47:19 +0300

    function to retrieve one movie

--- controllers/movie_controller.go
+++ controllers/movie_controller.go
@@ -1,2 +1,35 @@
 package controllers
 
+import (
+	"context"
+	"net/http"
+	"time"
+	"github.com/gin-gonic/gin"
+	"github.com/jimmymuthoni/movies-stream/database"
+	"github.com/jimmymuthoni/movies-stream/models"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+)
+
+var movieCollection *mongo.Collection = database.OpenCollection("movies")
+
+func GetMovies(client *mongo.Client) gin.HandlerFunc{
+	return func(c *gin.Context) {
+		ctx,cancel := context.WithTimeout(c, 100*time.Second)
+		defer cancel()	
+		cursor, err := movieCollection.Find(ctx, bson.D{})
+		if err != nil{
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch movies"})
+		}
+		defer cursor.Close(ctx)
+
+		var movies []models.Movie
+		if err = cursor.All(ctx, &movies); err != nil{
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to decode movies"})
+			return 
+		}
+
+		c.JSON(http.StatusOK, movies)
+
+	}
+}
