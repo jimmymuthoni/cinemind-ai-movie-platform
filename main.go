2794dcb18f3fe828110a35a7ffd9f22af8461176
jimmymuthoni <jimmymuthoni26@gmail.com>
2025-10-06 15:01:54 +0300

    local changes before rebase

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	controller "github.com/jimmymuthoni/movies-stream/controllers"
 )
 
 
@@ -13,6 +14,10 @@ func main(){
 	router.GET("/hello", func (c *gin.Context) {
 		c.String(200, "Hello, MovieStream")
 	})
+
+	router.GET("/movies", controller.GetMovies())
+
+	router.GET("/movie/:imdb_id", controller.GetMovie())
 
 	if err := router.Run(":8080"); err != nil{
 		fmt.Println("Failed to start server: ", err)
